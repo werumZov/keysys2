e644d397441aeef47eddd736ca0209bf69ea28d8
werumZov <nikitazhelezhnechenko2011@gmail.com>
2025-03-05 15:59:13 +0300

    Update KeySys2.lua

--- KeySys2.lua
+++ KeySys2.lua
@@ -41,7 +41,7 @@ function NektoHub()
 
 local Window = Rayfield:CreateWindow({
     Name = "KeySystem 2.0",
-    LoadingTitle = "Loading",
+    LoadingTitle = "Welcome!",
     LoadingSubtitle = "by werumZov",
     ConfigurationSaving = {
        Enabled = true,
@@ -56,15 +56,16 @@ local Window = Rayfield:CreateWindow({
     KeySystem = true, -- Set this to true to use our key system
     KeySettings = {
        Title = "Key System for NektoHub(check 1)",
-       Subtitle = "Key System",
-       Note = "No method of obtaining the key is provided",
+       Subtitle = "yoooooooooo",
+       Note = "Pls check this telegram channel @KeysRB",
        FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
        SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
        GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
        Key = {"check2"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
     }
  })
- local CheckTab = Window:CreateTab("Main", 4483362458) -- Title, Image
+ local CheckTab = Window:CreateTab("Main", 4483362458)
+ local CheckTab = Window:CreateTab("XD", 4483362458)-- Title, Image
  local Button = CheckTab:CreateButton({
     Name = "Start Nekto Hub",
     Callback = function()
